83cb364da17d4d231bd685400b7f2ef39c6a0317
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-04-22 18:13:00 +0000

    Add code

---
+++ src/main/java/Chair.java
@@ -0,0 +1,49 @@
+public class Chair {
+    //instance variables once again
+    private int legs;
+    private String material;
+
+    /**
+     * Constructor taking no parameters, sets legs to 4 and material to wood
+     * For the lazy
+     */
+    public Chair() {
+        this.legs = 4;
+        this.material = "wood";
+    }
+
+    /**
+     * Constructor taking legs of the chair only, sets material to wood
+     * @param legs Number of legs for the chair as an int
+     */
+    public Chair(int legs) {
+        this.legs = legs;
+        this.material = "wood";
+    }
+
+    /**
+     * Constructor taking legs of the chair and material of the chair
+     * @param legs Number of legs for the chair as an int
+     * @param material Material of the chair as a string
+     */
+    public Chair(int legs, String material) {
+        this.legs = legs;
+        this.material = material;
+    }
+
+    /**
+     * Getter for number of legs on the chair
+     * @return Number of legs on the chair as an int
+     */
+    public int getLegs() {
+        return this.legs;
+    }
+
+    /**
+     * Getter for material of the chair
+     * @return Material of the chair as a string
+     */
+    public String getMaterial() {
+        return this.material;
+    }
+}
---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,49 @@
+public class Rectangle {
+    //instance variables, as usual
+    private double length;
+    private double width;
+
+    /**
+     * Constructor taking no parameters, sets length to 4 and width to 8
+     * Wow you're lazy for using this
+     */
+    public Rectangle() {
+        this.length = 4;
+        this.width = 8;
+    }
+
+    /**
+     * Constructor taking only length, sets width to 8
+     * @param length Length of the rectangle as a double
+     */
+    public Rectangle(double length) {
+        this.length = length;
+        this.width = 8;
+    }
+
+    /**
+     * Constructor, takes length and width
+     * @param length Length of the rectangle as a double
+     * @param width Width of the rectangle as a double
+     */
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    /**
+     * Getter for rectangle length
+     * @return Length of the rectangle as a double
+     */
+    public double getLength() {
+        return this.length;
+    }
+
+    /**
+     * Getter for rectangle width
+     * @return Width of the rectangle as a double
+     */
+    public double getWidth() {
+        return this.width;
+    }
+}
